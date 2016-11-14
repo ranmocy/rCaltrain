c3dfa09361c389f62245942f10f240f8982db0fa
Ranmocy Sheng <ranmocy@gmail.com>
2016-11-13 19:50:13 -0800

    fix time display issue

--- android/app/src/main/java/me/ranmocy/rcaltrain/models/DayTime.java
+++ android/app/src/main/java/me/ranmocy/rcaltrain/models/DayTime.java
@@ -33,7 +33,7 @@ public class DayTime implements Comparable<DayTime> {
     @Override
     public String toString() {
         return String.format(Locale.getDefault(), "%02d:%02d",
-                minutesSinceMidnight / 60 % 24, minutesSinceMidnight % 60 % 24);
+                minutesSinceMidnight / 60 % 24, minutesSinceMidnight % 60);
     }
 
     /**
