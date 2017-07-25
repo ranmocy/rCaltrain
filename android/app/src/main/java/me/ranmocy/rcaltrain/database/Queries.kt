6cdd1dfc3fa299596ab3a47a8b41c04fa4d305f2
Ranmocy Sheng <ranmocy@gmail.com>
2017-07-24 22:32:34 -0700

    remove WITH from query statement since it is only since sqlite 3.8.3 which is sdk 21 or above

--- android/app/src/main/java/me/ranmocy/rcaltrain/database/Queries.kt
+++ android/app/src/main/java/me/ranmocy/rcaltrain/database/Queries.kt
@@ -2,69 +2,36 @@ package me.ranmocy.rcaltrain.database
 
 import me.ranmocy.rcaltrain.database.ScheduleDao.*
 
+private const val ADD_SERVICE_IDS = "(SELECT service_id FROM service_dates WHERE date = :now AND type = 1)"
+private const val REMOVE_SERVICE_IDS = "(SELECT service_id FROM service_dates WHERE date = :now AND type = 2)"
+private const val FROM_STATION_ID = "(SELECT id FROM stations WHERE name = :from)"
+private const val TO_STATION_ID = "(SELECT id FROM stations WHERE name = :to)"
+private const val FROM_SEQUENCE = "(SELECT sequence FROM stops WHERE trip_id = trips.id AND station_id in $FROM_STATION_ID LIMIT 1)"
+private const val TO_SEQUENCE = "(SELECT sequence FROM stops WHERE trip_id = trips.id AND station_id in $TO_STATION_ID LIMIT 1)"
+
+// WITH was introduced in sqlite 3.8.3: https://stackoverflow.com/questions/24036310/error-using-sql-with-clause
+// sqlite versions: https://stackoverflow.com/questions/2421189/version-of-sqlite-used-in-android
 const val QUERY = """
-WITH
-available_service_ids AS
-  (SELECT id FROM services WHERE
-    (
-      (weekday AND :serviceType = $SERVICE_WEEKDAY)
-      AND
-      (saturday AND :serviceType = $SERVICE_SATURDAY)
-      AND
-      (sunday AND :serviceType = $SERVICE_SUNDAY)
-    )
-    AND
-    (
-      id IN (SELECT service_id FROM service_dates WHERE date = :now and type = 1)
-      OR
-      (
-        id NOT IN (SELECT service_id FROM service_dates WHERE date = :now and type = 2)
-        AND start_date <= :now
-        AND end_date >= :now
-      )
-    )
-  )
-,
-
-from_station_ids AS
-  (SELECT id FROM stations WHERE name = :from)
-,
-
-to_station_ids AS
-  (SELECT id FROM stations WHERE name = :to)
-,
-
-target_trip_ids AS
-  (
-    SELECT id
-    FROM trips
-    WHERE service_id in available_service_ids
-      AND
-        (SELECT sequence
-         FROM stops
-         WHERE trip_id = trips.id AND station_id in from_station_ids
-         LIMIT 1) NOT NULL
-      AND
-        (SELECT sequence
-         FROM stops
-         WHERE trip_id = trips.id AND station_id in to_station_ids
-         LIMIT 1) NOT NULL
-      AND
-        (SELECT sequence
-         FROM stops
-         WHERE trip_id = trips.id AND station_id in from_station_ids
-         LIMIT 1)
-        <
-        (SELECT sequence
-         FROM stops
-         WHERE trip_id = trips.id AND station_id in to_station_ids
-         LIMIT 1)
-  )
-
 SELECT f.time AS departureTime, t.time AS arrivalTime
 FROM stops as f, stops as t
-WHERE f.station_id in from_station_ids
-  AND t.station_id in to_station_ids
+WHERE f.station_id IN $FROM_STATION_ID
+  AND t.station_id IN $TO_STATION_ID
   AND f.trip_id = t.trip_id
-  AND f.trip_id IN target_trip_ids
+  AND f.trip_id IN (
+    SELECT id
+    FROM trips
+    WHERE
+      service_id in (
+        SELECT id FROM services
+        WHERE
+          (CASE :serviceType WHEN $SERVICE_WEEKDAY THEN weekday WHEN $SERVICE_SATURDAY THEN saturday WHEN $SERVICE_SUNDAY THEN sunday ELSE NULL END)
+          AND (
+            id IN $ADD_SERVICE_IDS
+            OR (id NOT IN $REMOVE_SERVICE_IDS AND start_date <= :now AND end_date >= :now )
+          )
+      )
+      AND $FROM_SEQUENCE NOT NULL
+      AND $TO_SEQUENCE NOT NULL
+      AND $FROM_SEQUENCE < $TO_SEQUENCE
+  )
 """
